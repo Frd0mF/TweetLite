35d4dde4ac7f0a12a69164c55f3f14c6e5b6e9da
unknown <ibrahimbouslime@gmail.com>
2023-04-29 17:45:57 +0100

    Added new files for single post and profile view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,7 @@ import Image from "next/image";
 import { PostLoadingSkeleton } from "~/components/PostLoadingSkeleton";
 import { UserLoadingSkeleton } from "~/components/UserLoadingSkeleton";
 import { useRef, useState, KeyboardEvent } from "react";
+import Link from "next/link";
 
 const CreatePostWizard = () => {
   const [input, setInput] = useState<string>("");
@@ -74,7 +75,7 @@ const CreatePostWizard = () => {
         maxLength={255}
         disabled={isPosting}
         placeholder="What's on your mind?"
-        className="h-32 grow resize-none rounded-md border border-slate-200 bg-transparent px-2 outline-slate-700"
+        className="h-min grow resize-none self-end rounded-md border border-slate-200 bg-transparent px-2 outline-slate-700 focus:h-32 focus:border-transparent focus:ring-2 focus:ring-slate-500"
         value={input}
         onChange={(e) => setInput(e.target.value)}
       />
@@ -116,23 +117,31 @@ const PostView = (props: postWithUser) => {
   const { post, author } = props;
   if (!post || !author) return null;
   return (
-    <div className="flex items-start gap-3 border-b border-slate-500 p-4">
-      <Image
-        width={56}
-        height={56}
-        className="h-14 w-14 rounded-full"
-        src={author.profileImageUrl}
-        alt={`${author.username || ""} profile image`}
-      />
-      <div className="flex flex-col">
-        <div className="flex gap-1 font-semibold text-slate-300">
-          <span>{`@${author.username || ""}`}</span>
-          <span>·</span>
-          <span className="font-light">{dayjs(post.createdAt).fromNow()}</span>
+    <Link href={`/post/${post.id}`}>
+      <div className="flex items-start gap-3 border-b border-slate-500 p-4 hover:bg-slate-800 hover:transition-colors">
+        <Image
+          width={56}
+          height={56}
+          className="h-14 w-14 rounded-full"
+          src={author.profileImageUrl}
+          alt={`${author.username || ""} profile image`}
+        />
+        <div className="flex flex-col">
+          <div className="flex gap-1 font-semibold text-slate-300">
+            <Link href={`/@${author.username}`}>
+              <span className="hover:underline">{`@${
+                author.username || ""
+              }`}</span>
+            </Link>
+            <span>·</span>
+            <span className="font-light">
+              {dayjs(post.createdAt).fromNow()}
+            </span>
+          </div>
+          <span className="break-all text-2xl">{post.content}</span>
         </div>
-        <span className="break-all text-2xl">{post.content}</span>
       </div>
-    </div>
+    </Link>
   );
 };
 
